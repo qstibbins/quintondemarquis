d36c6de5d66e74232e3659d8481a392610ceda4c
qstibbins <quinton.stibbins@gmail.com>
2025-06-14 18:13:48 -0500

    Adding tailwind and header component scsffolding

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+// import '../App.css'
+
+function Contact() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default Contact
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav>
+      <h1>Quinton Stibbins</h1>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
+// This is a simple header component for a React application.
